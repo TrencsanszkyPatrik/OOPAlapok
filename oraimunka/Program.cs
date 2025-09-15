b8aa9639d3adc68e138dcb1eef66f1a825003d9e
TrencsanszkyPatrik <trencsanszkyp@kkszki.hu>
2025-09-15 08:10:32 +0200

    Feladat3 is ready to use

--- oraimunka/Program.cs
+++ oraimunka/Program.cs
@@ -11,12 +11,29 @@ namespace oraimunka
 
         public class Szemely
         {
-            private string Nev;
-            private int Eletkor;
+            private string _nev;
+            private int _eletkor;
 
-            public Szemely(string nev, int eletkor)
+            public string Nev
             {
-                Nev = nev; Eletkor = eletkor;
+                get { return _nev; } //privat adattag kiolvasásához
+                set { _nev = value; }
+            }
+
+            public int Eletkor
+            {
+                get { return _eletkor; }
+                set
+                {
+                    if (value > 0) _eletkor = value;
+                    else Console.WriteLine("Nem lehet negatív.");
+                }
+            }
+
+            /*
+            public Szemely(string neve, int eletkora)
+            {
+                nev = neve; eletkor = eletkora;
             }
 
             public string Kiir()
@@ -24,13 +41,20 @@ namespace oraimunka
                 return $"A személy neve: {Nev}\nÉletkora: {Eletkor}";
 
             }
-        } 
+            
+            */
+        }
 
 
         static void Main(string[] args)
         {
-            Szemely person = new Szemely("Dani", 67);
-            Console.WriteLine(person.Kiir());
+            Szemely person = new Szemely();
+            person.Eletkor = 33;
+            person.Nev = "Feri";
+            Console.WriteLine($"A személy neve: {person.Nev}, kora: {person.Eletkor}");
+            
+            //Szemely person = new Szemely("Dani", 67);
+            //Console.WriteLine(person.Kiir());
         }
     }
 }
